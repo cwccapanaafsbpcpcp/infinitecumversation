b87ba4f28777f5d5a3c5f4a0255ef652ea94ff3a
B2L401NbK <CCPBN@RAICK.com>
2022-11-29 00:26:07 -0500

    Fixed ffmpeg bug, improved file prep ffmpeg reads from file vice string, input filename preserved on output

--- main.py
+++ main.py
@@ -6,68 +6,14 @@ import json
 # Token: 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
 
 
-#Fuck all this Indian-written shit I stole
-def prep_file():
-    spacermilli = 2000
-    spacer = AudioSegment.silent(duration=spacermilli)
-    audio = AudioSegment.from_mp3("input.mp3") 
-    audio = spacer.append(audio, crossfade=0)
-    audio.export('input_prep.wav', format='wav')
 
-# def generate_diarization():  
-#     pipeline = Pipeline.from_pretrained('pyannote/speaker-diarization', use_auth_token='')
-#     DEMO_FILE = {'uri': 'blabla', 'audio': 'input_prep.wav'}
-#     dz = pipeline(DEMO_FILE)  
-#     with open("diarization.txt", "w") as text_file:
-#         text_file.write(str(dz))
 
-# def millisec(timeStr):
-#   spl = timeStr.split(":")
-#   s = (int)((int(spl[0]) * 60 * 60 + int(spl[1]) * 60 + float(spl[2]) )* 1000)
-#   return s
+def prep_file(input_filename):
+    audio = AudioSegment.from_mp3(input_filename) 
+    output_filename = input_filename.replace('.mp3', '.wav')
+    audio.export(output_filename, format='wav')
+    return output_filename
 
-# def group_diarization():
-#     dzs = open('diarization.txt').read().splitlines()
-#     groups = []
-#     g = []
-#     lastend = 0
-
-#     for d in dzs:   
-#         if g and (g[0].split()[-1] != d.split()[-1]):      #same speaker
-#             groups.append(g)
-#             g = []
-    
-#         g.append(d)
-    
-#         end = re.findall('[0-9]+:[0-9]+:[0-9]+\.[0-9]+', string=d)[1]
-#         end = millisec(end)
-#         if (lastend > end):       #segment engulfed by a previous segment
-#             groups.append(g)
-#             g = [] 
-#         else:
-#             lastend = end
-#     if g:
-#         groups.append(g)
-
-#     return groups
-
-# def save_groups(groups):
-#     audio = AudioSegment.from_wav("input_prep.wav")
-#     for e,g in enumerate(groups):
-#         spt = g[0].split(' ')
-#         st = datetime.strptime(spt[1], '%H:%M:%S.%f')
-#         et = datetime.strptime(spt[1], '%H:%M:%S.%f]')
-#         print(st)
-#         segment, speaker = g[0].split(' ')[-2:]
-#         start_ts = re.findall('[0-9]+:[0-9]+:[0-9]+\.[0-9]+', string=g[0])[0]
-#         end_ts = re.findall('[0-9]+:[0-9]+:[0-9]+\.[0-9]+', string=g[-1])[1]
-#         start_ts = millisec(start_ts)
-#         end_ts = millisec(end_ts)
-#         audio[start_ts:end_ts].export(str(e) + '.wav', format='wav')
-# prep_file()
-# generate_diarization()
-# dzs = group_diarization()
-# save_groups(dzs)
 
 # I have the start/ends of when speaker is speaking, ffmpeg needs when they aren't speaking. There's an easier way somehow
 def generate_mute_filters(dzs, speaker): 
@@ -84,24 +30,30 @@ def generate_file(input_file, dzs, speaker):
     mute_filters = '+'.join(generate_mute_filters(dzs, speaker))
     input_prefix = input_file.rstrip('.wav')
     filtered_filename = f'{input_prefix}_{speaker}.wav'
-    ffmpeg_cmd = f'ffmpeg -y -i {input_file} -af volume=0:enable=\'{mute_filters}\' {filtered_filename}'
+    with open('filter.txt', 'w') as f:
+        f.write(f'volume=0:enable=\'{mute_filters}\'') # required on windows due to command line char limit
+    ffmpeg_cmd = f'ffmpeg -y -i {input_file} -filter_script:a filter.txt {filtered_filename}'
     os.system(ffmpeg_cmd)
     return filtered_filename
 
 def transcribe(filename):
-    model = whisper.load_model('medium.en')
+    out_file = filename.rstrip('.wav') + '.json'
+    model = whisper.load_model('base.en')
     result = model.transcribe(filename)
-    print(result)
+    
+    with open(out_file, 'w') as f:
+        json.dump(result, f)
 
 
 def main():
-    input_file = 'trim.wav'
+    input_file = prep_file('input.mp3')
     print('loading pipeline')
     pipeline = Pipeline.from_pretrained('pyannote/speaker-diarization', use_auth_token='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')
     print('diarizing')
     diarization = pipeline(input_file)
     dzs = [(i[2], i[0].start, i[0].end) for i in diarization.itertracks(yield_label=True)]
     speakers = set([i[0] for i in dzs])
+    print(f'found {len(speakers)} speakers')
     dzs = sorted(dzs, key=lambda dz: dz[1])
     for speaker in speakers:
         print(f'filtering {speaker}')
